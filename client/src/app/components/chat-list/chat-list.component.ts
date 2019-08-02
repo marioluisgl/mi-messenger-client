4bbde30b2f3983d184aad6ed33d0478830db0aa9
marioluisgl <ml.garcia.lugones@gmail.com>
2019-08-02 16:16:28 +0200

    creando lista de usuarios

--- client/src/app/components/chat-list/chat-list.component.ts
+++ client/src/app/components/chat-list/chat-list.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/user';
+import { UserService } from 'src/app/services/user.service';
+import { SocketService } from 'src/app/services/socket.service';
+import { ChatService } from 'src/app/services/chat.service';
+import { DataShareService } from 'src/app/services/data-share.service';
+import { Router } from '@angular/router';
+
+
+import { ChatListResponse } from 'src/app/interfaces/chat-list-response';
 
 @Component({
   selector: 'app-chat-list',
@@ -7,9 +16,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatListComponent implements OnInit {
 
-  constructor() { }
+  loading = true;
+	userId: string = null;
+	selectedUserId: string = null;
+  chatListUsers: User[] = [];
+  
+  constructor(
+    private _userService: UserService,
+    private _socketService: SocketService,
+    private _chatService: ChatService,
+    private _dataShareService: DataShareService,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+    this.loading = true;
+    this.userId = this._dataShareService.getUserId();
+    this._socketService.getChatList(this.userId).subscribe((chatListResponse: ChatListResponse)=>{
+      this.renderChatList(chatListResponse);
+      console.log(this.chatListUsers);
+    });
+  }
+
+
+  renderChatList(chatListResponse: ChatListResponse): void {
+    if(!chatListResponse.error){
+
+      if(chatListResponse.singleUser){
+
+        if(this.chatListUsers.length > 0){
+          this.chatListUsers = this.chatListUsers.filter((obj: User)=>{
+            return obj.id !== chatListResponse.chatList[0].id;
+          });
+        }
+
+        this.chatListUsers = this.chatListUsers.concat(chatListResponse.chatList);
+
+      }else if(chatListResponse.userDisconnected){
+
+        const loggedOutUser = this.chatListUsers.findIndex((obj: User)=> obj.id === chatListResponse.userid);
+        if(loggedOutUser >=0){
+          this.chatListUsers[loggedOutUser].online = 'false';
+        }
+
+      }else{
+        this.chatListUsers = chatListResponse.chatList;
+      }
+      this.loading = false;
+    }else{
+      alert(`Unable to load Chat list, Redirecting to Login.`);
+			this._userService.removeLSdata()
+				.then(async (removedLs: boolean) => {
+					await this._router.navigateByUrl('/home');
+					this.loading = false;
+				})
+				.catch(async (error: Error) => {
+					alert(' This App is Broken, we are working on it. try after some time.');
+					await this._router.navigate(['/']);
+					console.warn(error);
+					this.loading = false;
+				});
+    }
+
+
   }
 
 }
--- client/src/app/services/socket.service.ts
+++ client/src/app/services/socket.service.ts
@@ -8,6 +8,7 @@ import { Auth } from '../interfaces/auth';
 import { ChatListResponse } from '../interfaces/chat-list-response';
 import { MessageSocketEvent } from '../interfaces/message-socket-event';
 import { Message } from '../interfaces/message';
+import { Observable } from 'rxjs';
 /* importing interfaces ends */
 
 
@@ -28,6 +29,23 @@ export class SocketService {
   */
   connectSocket(userId: string): void {
     this.socket = io(this.SOCKET_URL, { query: `userId=${userId}` });
+  }
+
+  /*
+	* Method to receive chat-list-response event.
+  */
+  getChatList(userId: string = null): Observable<ChatListResponse>{
+    if(userId !== null){
+      this.socket.emit('chat-list', {userId: userId});
+    }
+    return new Observable(observer =>{
+      this.socket.on('chat-list-response', (data: ChatListResponse)=> {
+        observer.next(data);
+      });
+      return () =>{
+        this.socket.disconnect();
+      };
+    });
   }
 
 
